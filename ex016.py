afbbf128e6c2245974e76c5e5c2f1554f9f5c422
Edimar Moro <brizola182738@gmail.com>
2026-06-10 19:58:54 -0300

    ex 016

--- ex016.py
+++ ex016.py
@@ -1,3 +1,16 @@
-ex016
+#import math#
+#num = float(input('Digite um valor:'))#
+#print('O valor digitado foi {} e a sua porção inteira é {}'.format (num, math.trunc(num)))#
 
-                         ''))
+###----------------------------###------------------------------------##
+
+#from math import trunc
+#num = float(input('Digite um valor:'))
+#print('O valor digitado foi {} e sua porção inteira é {}'.format(num, trunc(num)))
+
+####--------------------------###-----------------------------------##
+num = float(input('Digite um valor:'))
+print('O valor digitado foi {} e sua porção inteira é {}'.format(num, int(num)))
+
+
+
