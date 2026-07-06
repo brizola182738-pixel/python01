103148ce60fd260e03935075887deecc06b3f742
Edimar Moro <brizola182738@gmail.com>
2026-07-06 20:19:03 -0300

    ex026.py

--- ex026.py
+++ ex026.py
@@ -2,3 +2,4 @@ frase = str(input('Digite um texto:')) .upper().strip()
 print('A letra A aparece {} vezes no texto.'.format(frase.count('A')))
 print(' A primeira letra A apareceu na posição {}'.format(frase.find('A')+1))
 print('A ultima letra A apareceu na posição {}'.format(frase.rfind('A')+1))
+
