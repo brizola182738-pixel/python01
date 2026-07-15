af6d954c9068282736a458fef1c9231822419d1a
Edimar Moro <brizola182738@gmail.com>
2026-07-14 21:17:03 -0300

    aula 011

---
+++ aula 11 adicionando cores.py
@@ -0,0 +1,8 @@
+a = 3
+b = 5
+print('Os valores são \033[34m{}\033[m e \033[32m{}\033[m'.format(a, b))
+
+
+nome = 'Edimar'
+print('Olá! Prazer em te conhecer, {}{}{}!!'.format('\033[;34m', nome, '\033[m'))
+
