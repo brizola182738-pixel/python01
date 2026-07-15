14aa4839bc6fb48e8851aab69c5f345495d84743
Edimar Moro <brizola182738@gmail.com>
2026-07-15 20:59:52 -0300

    ex 037

---
+++ ex038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Primeiro numero:'))
+n2 = int(input('Segundo numero:'))
+if n1 > n2:
+    print('O PRIMEIRO numero é maior.')
+elif n2 > n1:
+    print('O SEGUNDO numero é maior.')
+else:
+    print('Os NUMEROS são iguais.')
+
