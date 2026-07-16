f660df86897fe67f0c3fbed965ea1e497a5d877e
Edimar Moro <brizola182738@gmail.com>
2026-07-15 23:45:34 -0300

    ex 040

--- ex040.py
+++ ex040.py
@@ -1,9 +1,10 @@
 n1 = float(input('Primeira nota?'))
 n2 = float(input('Segunda nota?'))
 media = (n1 + n2) / 2
-if media >= 7.0:
-    print(' Parábens você esta aprovado! sua media foi {:.2f}'.format(media))
-elif media > 5.0 < 6.9:
-    print('Você esta de recuperação sua media foi {:.2f}'.format(media))
-else:
-    print('você esta reprovado! sua media foi {:.2f}'.format(media))
+print('Tirando {:.1f} e {:.1f}, a media do aluno será {:.1f}'.format(n1, n2, media))
+if media >= 7.0 :
+    print(' Parábens você esta APROVADO!')
+elif media > 5.0 < 7.0:
+    print('Você esta de RECUPERAÇÃO!')
+elif media <5.0:
+    print('você esta REPROVADO!')
