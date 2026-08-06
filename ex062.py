60d335b0a5b90c671105dcdb6d8ba16c3f1b57dc
Edimar Moro <brizola182738@gmail.com>
2026-08-05 22:49:21 -0300

    ex 062

---
+++ ex062.py
@@ -0,0 +1,16 @@
+primeiro = int(input('Digite o primeiro termo:'))
+razão = int(input('Digite a razão:'))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= 10:
+        print('{} -> '.format(termo), end='')
+        termo += razão
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostra a mais ?'))
+print('Progressão encerrada com {} termos mostrados'.format(total))
+
